f5733953ae66e3d042eb73121e23bb3c912e6193
sakthi1617 <sakthivel.m@colanonline.com>
2024-01-19 12:43:49 +0530

    Add Authentication controller

---
+++ InstacartAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstacartAPI.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
