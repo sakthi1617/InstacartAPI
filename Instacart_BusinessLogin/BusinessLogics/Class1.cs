af9c8db81ad6cf313403aea4411810b3dc394c0b
sakthi1617 <sakthivel.m@colanonline.com>
2024-01-22 11:51:29 +0530

    Add class  for businesslogic

--- Instacart_BusinessLogin/BusinessLogics/Class1.cs
+++ Instacart_BusinessLogin/BusinessLogics/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Instacart_DataAccess.DBContext
+namespace Instacart_BusinessLogic.BusinessLogics
 {
-    internal class InstaContext
+    internal class Class1
     {
     }
 }
