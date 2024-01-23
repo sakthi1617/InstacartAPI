1153bc52717e725de75abbd722562850db35327a
Arjunkumar S <arjunkumar.s@colanonline.com>
2024-01-23 19:35:54 +0530

    program.cs

--- InstacartAPI/Program.cs
+++ InstacartAPI/Program.cs
@@ -1,4 +1,8 @@
+using Instacart_BusinessLogic.BusinessLogics;
+using Instacart_BusinessLogic.IBusinessLogics;
 using Instacart_DataAccess.Data;
+using Instacart_DataAccess.IService;
+using Instacart_DataAccess.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +12,12 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<DB_context>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddTransient<IAdminBusinessLogic, AdminBusinessLogic>();
+builder.Services.AddTransient<IAdminService, AdminService>();
+builder.Services.AddTransient<IAuthservice, AuthServices>();
+builder.Services.AddTransient<IAuthBLL, AuthBLL>();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
