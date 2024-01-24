82d3878835c2aedd179ba108f8602d714ab61834
GHarisanthosh <harisanthosh.g@colanonline.com>
2024-01-24 18:13:14 +0530

    Product table CRUD

---
+++ Instacart_BusinessLogin/IBusinessLogics/IProductBLL.cs
@@ -0,0 +1,19 @@
+﻿using Instacart_BusinessLogic.SupportModels;
+using Instacart_DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instacart_BusinessLogic.IBusinessLogics
+{
+    public interface IProductBLL 
+    {
+        public Task<ResponseStatus<Product>>CreateProduct(Product product);
+        public Task<ResponseStatus<Product>>UpdateProducts(Guid productid, Product model);
+        public Task<ResponseStatus<int>> DeleteProduct(Guid productid);
+        public Task<ResponseStatus<Product>> GetProductByID(Guid productid);
+        public Task<ResponseStatus<Product>> GetProducts();
+    }
+}
--- Instacart_DataAccess/Models/Product.cs
+++ Instacart_DataAccess/Models/Product.cs
@@ -12,14 +12,14 @@ namespace Instacart_DataAccess.Models
         public int CategoryID { get; set; }    
         public int SubCategoryID { get; set; }
         public int BrandID { get; set;}
-        public string ProductName { get; set; } 
+        public string? ProductName { get; set; } 
         public decimal Price { get; set; }
         public int MeasurementID { get; set; }
         public decimal Measurement { get; set; }
-        public string Description { get; set; }
-        public string Directions { get; set; }
-        public string Warning { get; set; }
-        public string Ingredients { get; set; }
+        public string? Description { get; set; }
+        public string? Directions { get; set; }
+        public string? Warning { get; set; }
+        public string? Ingredients { get; set; }
         public int StockCount { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set;}
@@ -30,6 +30,6 @@ namespace Instacart_DataAccess.Models
         public int OfferID { get; set; }
         public decimal DiscountAmount { get; set; }
 
-
+        
     }
 }
